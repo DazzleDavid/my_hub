a73228aa610d8f5a9d574a82f3fb7980e72ae952
DazzleDavid <david700707@gmail.com>
2026-07-24 18:50:10 +0800

    Fix router base path for github pages

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -11,7 +11,9 @@ import Login from "@/pages/Login/Login";
 import Dashboard from "@/pages/Dashboard/Dashboard";
 
 import ProtectedRoute from "./ProtectedRoute";
+import { HashRouter } from "react-router-dom";
 import { Navigate } from "react-router-dom";
+
 
 
 export default function AppRouter(){
@@ -19,7 +21,7 @@ export default function AppRouter(){
 
 return (
 
-<BrowserRouter>
+<HashRouter>
 
 
 <Routes>
@@ -55,7 +57,7 @@ element={
 </Routes>
 
 
-</BrowserRouter>
+</HashRouter>
 
 )
 
